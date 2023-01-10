777b23fbbbe51393b37428e3c734537b6e8dccd5
Melondad39 <52050883+Melondad39@users.noreply.github.com>
2023-01-10 16:29:19 +0100

    work

---
+++ server/modules/avatar.js
@@ -0,0 +1,15 @@
+/*
+ * Nom : Avatar
+ * Description : GEstion des avatar
+ * Auteur(s) : Clémentine Desrucques - Enzo Hardouin
+ */
+
+// Définit les méthodes "publiques" (utilisation à l'extérieur du module)
+export default {
+    getRandomAvatar: getRandomAvatar
+}
+
+/* Get aléatoire avatar */
+function getRandomAvatar() {
+    return "<span style='color:red'>OK</span>";
+}
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var fs = require('fs');			// Accès au système de fichier
 
 // Chargement des modules perso
 var daffy = require('./modules/daffy.js');
+var avatar = require('./modules/avatar.js');
 
 // Initialisation du serveur HTTP
 var app = express();
@@ -42,7 +43,7 @@ io.sockets.on('connection', function(socket)
 		message = ent.encode(message);
 		
 		// Transmet le message à tous les utilisateurs (broadcast)
-		io.sockets.emit('new_message', {name:socket.name, message:message});
+		io.sockets.emit('new_message', {name:socket.name, message:message, avatar:avatar.getRandomAvatar()});
 		
 		// Transmet le message au module Daffy (on lui passe aussi l'objet "io" pour qu'il puisse envoyer des messages)
 		daffy.handleDaffy(io, message);
