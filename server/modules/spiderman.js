2a70043a046c8ecc8cddec4bd3c1f4259041dee6
Clementine DESRUCQUES <cd854530@iut21.u-bourgogne.fr>
2023-01-16 15:10:50 +0100

    Module Spiderman

--- server/modules/spiderman.js
+++ server/modules/spiderman.js
@@ -17,16 +17,40 @@ function handleSpider(io, message)
 {
 	// Passe le message en minuscules (recherche insensible à la casse)
 	message = message.toLowerCase();
-	
+	io.sockets.emit('new_message',
+		{
+			name:'Spiderman',
+			message:'<span class="spiderman"> test4</span>',
+			avatar:"<img src='/modules/avatar/spiderman.svg' alt='Spiderman avatar' width='30px'>"
+		});
+	messagePerso('test1');
 	// Est-ce qu'il contient une référence à Spiderman ?
 	if (message.includes('spiderman'))
 	{
-		// Si oui, envoie la réponse de Spiderman...
-		io.sockets.emit('new_message',
+		messagePerso('test2');
+			//Autre réponse de Spiderman
+		if (message.includes('calcul'))
+		{
+			// Si oui, envoie la réponse de Spiderman...
+			messagePerso('Les maths.');
+		}
+		else if (message.includes('test'))
+		{
+			messagePerso('test');
+		} else 
+		{
+			messagePerso('Un grand pouvoir implique de grandes responsabilitées.');
+		}
+	}
+	
+}
+
+function messagePerso(io, message)
+{
+	io.sockets.emit('new_message',
 		{
 			name:'Spiderman',
-			message:'<span class="spiderman"> Un grand pouvoir implique de grandes responsabilitées.</span>',
+			message:'<span class="spiderman"> ' + message + '</span>',
 			avatar:"<img src='/modules/avatar/spiderman.svg' alt='Spiderman avatar' width='30px'>"
 		});
-	}
 }
