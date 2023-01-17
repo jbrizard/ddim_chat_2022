83ef02e2e5409f32b60de1b9c6d3e7386f03d933
PatrickePatate <lucienpuget1@gmail.com>
2023-01-17 16:29:59 +0100

    Extraction des clés api météo dans .env + retouches module ratio

--- server/modules/ratio.js
+++ server/modules/ratio.js
@@ -12,41 +12,53 @@ module.exports = {
 
 function handleMessage(io, socket, message)
 {
-    if((process.env['ACTIVATE_RATIO_BOT']??false))
+    if(rules(socket,message))
     {
-        if(Math.floor(Math.random()*10) >= 7)
-        {
-            if(message.startsWith(socket.name) || message.startsWith('@'))
-                return;
-
-            sentiment(message).then((res)=>{
-                console.log("Résultat OPEN AI :");
-                console.log(res.data['choices'][0]);
-                if(res.data['choices'][0].text.includes('1'))
-                {
-                    const resps = [
-                        'Mange ce gros ratio sale looser',
-                        'AHAHA! T\'es trop une merde + big ratio sur toi',
-                        'Ton père a bien fait de partir quand tu étais encore enfant espèce de sous-humain. Ratio + longue vie à Booba.',
-                        'RATIO AHAHAHA',
-                        'Tu feras gaffe tu as laissé tombé ce ratio gros looser.'
-                    ]
-                    io.sockets.emit('new_message', {name:"Ratio", message:resps[Math.floor(Math.random()*resps.length)]});
-                }
-            }).catch((err)=>{
-                console.log(err);
-            })
-        }
+        sentiment(message).then((res)=>{
+            console.log("Résultat OPEN AI :");
+            console.log(res.data['choices'][0]);
+            if(res.data['choices'][0].text.includes('1'))
+            {
+                const resps = [
+                    'Mange ce gros ratio sale looser',
+                    'AHAHA! T\'es trop une merde + big ratio sur toi',
+                    'Ton père a bien fait de partir quand tu étais encore enfant espèce de sous-humain. Ratio + longue vie à Booba.',
+                    'RATIO AHAHAHA',
+                    'Tu feras gaffe tu as laissé tombé ce ratio gros looser.'
+                ]
+                io.sockets.emit('new_message', {name:"Ratio", message:resps[Math.floor(Math.random()*resps.length)]});
+            }
+        }).catch((err)=>{
+            console.log(err);
+        })
     }
+
+}
+
+function rules(socket, message)
+{
+    if((process.env['ACTIVATE_RATIO_BOT']??false))
+        return false;
+
+    if(message.split(' ').length < 3)
+        return false;
+
+    if(Math.floor(Math.random()*10) >= 7)
+        return false;
+
+    if(message.startsWith(socket.name) || message.startsWith('@'))
+        return false;
+
+    return true;
 }
 
 async function sentiment(message)
 {
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "In french, renvoi moi 1 si le message est triste, ou 0 si le message ne l'est pas : "+message,
-        temperature: 0,
-        max_tokens: 15,
+        prompt: "In french, Ce text fait-il preuve de tristesse ? Si oui, répond 1, si non, répond 0 : "+message,
+        temperature: 0.7,
+        max_tokens: 4,
         top_p: 1.0,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
