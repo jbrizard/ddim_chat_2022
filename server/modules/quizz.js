e0eae622ae22d1bc310b6f183c7967ec7159588e
Melondad39 <52050883+Melondad39@users.noreply.github.com>
2023-01-17 09:11:44 +0100

    work in progress

---
+++ server/modules/quizz.js
@@ -0,0 +1,30 @@
+module.exports = {
+    handleQuizz: handleQuizz
+}
+
+function handleQuizz(io, message){
+    message = message.toLowerCase();
+    if (message.includes('quizz') || message.includes('quiz') || io.sockets.quizz === true){
+        io.sockets.quizz = true;
+
+        if (message.includes('oui') || message.includes('yes')){
+
+        }else if (message.includes('non') || message.includes('no')){
+
+        } else if (message.includes('stop') || message.includes('fin')){
+            io.sockets.quizz = false;
+        }else {
+            messagePerso(io,'Veuillez répondre par oui ou non');
+        }
+    }
+}
+
+function messagePerso(io, message)
+{
+    io.sockets.emit('new_message',
+        {
+            name:'Spiderman',
+            message:'<span class="spiderman"> ' + message + '</span>',
+            avatar:"<img src='/modules/avatar/quizz.svg' alt='Spiderman avatar' width='30px'>"
+        });
+}
