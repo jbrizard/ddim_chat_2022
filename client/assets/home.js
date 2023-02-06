53adc214f6f5d700c9e0c660f6cf621355a854cd
Just1Sornay <jsornay21@gmail.com>
2023-02-06 10:13:08 +0100

    --

--- client/assets/home.js
+++
@@ -1,7 +0,0 @@
-function handleRedirectChat() {
-}
-buttonRedirectChat.addEventListener('click', handleRedirectChat);
-
-function handleContactChat() {
-}
-buttonRedirectContact.addEventListener('click', handleContactChat);
